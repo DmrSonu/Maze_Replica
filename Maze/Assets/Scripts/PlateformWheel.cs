5515076d36d526b853dbd31c8995ec5bea1f6208
DmrSonu <47378991+DmrSonu@users.noreply.github.com>
2019-08-07 18:52:50 +0530

    Giant wheel

---
+++ Maze/Assets/Scripts/PlateformWheel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateformWheel : MonoBehaviour
+{
+    [SerializeField] Vector3 rotatingAxis;
+    [SerializeField] int wheelSpeed;
+    [SerializeField] Transform[] wheelPoint;
+    [SerializeField] GameObject floatingPlatformPrefab;
+
+    GameObject[] rotatingPosition;
+
+    private void Start()
+    {
+        for(int i = 0; i < wheelPoint.Length; i++)
+        {
+            Debug.Log("ahfa");
+            rotatingPosition[i] = Instantiate(floatingPlatformPrefab) as GameObject;
+            //rotatingPosition[i].transform.position = wheelPoint[i].position;
+            Debug.Log("ahfa");
+        }
+        /*
+        for (int i = 0; i < wheelPoint.Length; i++)
+        {
+            Instantiate(floatingPlatformPrefab).transform.position = wheelPoint[i].position;
+        }*/
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Rotate(rotatingAxis * wheelSpeed);
+    }
+}
